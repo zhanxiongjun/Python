fea9a566b3f8083e8f3c8e872e491731adab4382
zhanxiongjun <zhanj0123@gmail.com>
2019-01-10 00:47:29 -0500

    修改字符串长度问题

--- ninth.py
+++ ninth.py
@@ -4,3 +4,12 @@ def not_string(str):
     else:
         return str
 print(not_string("not bad'"))
+
+"""但是这样做是错的，因为没有考虑到字符串长度小于三"""
+
+def not_string_two(str):
+    if len(str) < 3 or 'not' not in str[:3]
+        return 'not ' + str
+    else:
+        return str
+    
